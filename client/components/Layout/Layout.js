4602041e7995c58c83f79d7b47861f52b593949a
adrivelasco <adrivelasco96@hotmail.com>
2018-05-04 12:14:57 -0300

    feat: Layout to PureComponent

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -4,15 +4,18 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from '../AppBar';
 import styles from './Layout.styles';
 
-const Layout = ({ children, classes }) => {
-  return (
-    <div className={classes.root}>
-      <AppBar title="AV Tweetx" />
-      <div className={classes.content}>
-        {children}
+class Layout extends React.PureComponent {
+  render() {
+    const { children, classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar title="AV Tweetx" />
+        <div className={classes.content}>
+          {children}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default withStyles(styles)(Layout);

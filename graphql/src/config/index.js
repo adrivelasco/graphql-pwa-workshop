0bd0a37f02e46d532ad55ccddd208cff42249235
adrivelasco <adrivelasco96@hotmail.com>
2018-05-15 10:31:52 -0300

    feat: Add resolvers getting to API

--- graphql/src/config/index.js
+++ graphql/src/config/index.js
@@ -2,7 +2,12 @@
 
 const config = {
   port: process.env.PORT || '4000',
-  env: process.env.NODE_ENV || 'development'
+
+  env: process.env.NODE_ENV || 'development',
+
+  api: {
+    url: 'http://www.mocky.io/v2/5aa722ea2f0000e8048ea463'
+  }
 };
 
 module.exports = config;
--- graphql/src/resolvers/Query.js
+++ graphql/src/resolvers/Query.js
@@ -1,8 +1,36 @@
 'use strict';
 
+const rp = require('request-promise');
+
+const config = require('../config');
+
 const Query = {
-  hello: (source, args, ctx) => {
-    return `Hello ${args.name || 'world'}!`;
+  animals: async (source, args, ctx) => {
+    try {
+      const response = await rp({
+        method: 'GET',
+        uri: config.api.url,
+        json: true,
+        resolveWithFullResponse: true
+      });
+      return response.body;
+    } catch (err) {
+      return console.log(err);
+    }
+  },
+
+  animal: async (source, args, ctx) => {
+    try {
+      const response = await rp({
+        method: 'GET',
+        uri: config.api.url,
+        json: true,
+        resolveWithFullResponse: true
+      });
+      return response.body.find(({ name }) => name === args.name);
+    } catch (err) {
+      return console.log(err);
+    }
   }
 };
 
